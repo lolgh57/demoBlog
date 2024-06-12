5ff854301107001486c8cd6b4f503298394464f0
lolgh57 <sasha.over.game@gmail.com>
2024-06-12 23:10:05 +0200

    taro

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,18 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <section class="min-h-screen flex flex-col">
-        <LayoutHeader />
-        <div class="flex-grow w-full h-full bg-arcanum pt-[7.5rem]" :class="{'dark': $colorMode.value === 'dark', 'light': $colorMode.value === 'light'}">
-            <slot />
-        </div>
-    </section>
+  <section class="min-h-screen flex flex-col">
+    <LayoutHeader />
+    <div
+      class="flex-grow w-full h-full bg-arcanum pt-[7rem] md:pt-0"
+      :class="{
+        dark: $colorMode.value === 'dark',
+        light: $colorMode.value === 'light',
+      }"
+    >
+      <slot />
+    </div>
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
